05300a93b144edf5111271677ff0ed809622365b
Markinsz <markins.psm@gmail.com>
2024-05-08 15:01:10 -0300

    feat: ex5 for

---
+++ Loop/For/ex5.js
@@ -0,0 +1,10 @@
+// 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+
+let n1 = 0, n2 = 1, n;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(n1);
+    n = n1 + n2;
+    n1 = n2;
+    n2 = n;
+}
