6f8cfe01f363989add0beecd0220f0aee4f70dc1
Markinsz <markins.psm@gmail.com>
2024-05-08 16:21:38 -0300

    feat: ex 10 while

---
+++ Loop/While/ex10.js
@@ -0,0 +1,7 @@
+// 10. Faça um programa que receba um número do usuário e continue a pedir o número se o numero for diferente de 0.
+
+let n = prompt("Digite um número")
+
+while (n != 0) {
+    n = prompt("Digite outro número")
+}
