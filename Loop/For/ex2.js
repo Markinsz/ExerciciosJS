06b7debbd22b0a4842bc803fbdbbc6a2e9855fae
Markinsz <markins.psm@gmail.com>
2024-05-08 16:24:37 -0300

    feat: exercícios modificados

--- Loop/For/ex2.js
+++ Loop/For/ex2.js
@@ -1,5 +1,5 @@
 // Imprimir números pares de 1 a 10.
 
-for (let cont = 0; cont <= 10; cont+=2){
+for (let cont = 2; cont <= 10; cont+=2){
     console.log(cont);
 }
--- Loop/For/ex5.js
+++ Loop/For/ex5.js
@@ -1,6 +1,6 @@
 // 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
 
-let n1 = 0, n2 = 1, n;
+let n1 = 1, n2 = 1, n;
 
 for (let i = 1; i <= 10; i++) {
     console.log(n1);
